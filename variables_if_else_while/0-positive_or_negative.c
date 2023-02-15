30c348b9d95dc527b0951592e6b26833666b9272
root <Diego29012>
2023-02-15 09:54:58 -0800

    Automatic

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,17 @@
 
+
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
+/*
+ *  * main - Prints a random number and states whether
+ *   *        it is positive, negative, or zero.
+ *    *
+ *     * Return: Always 0.
+ *     */
 int main(void)
 {
-	/**
-    * main - Prints a random number and states whether
-    *  *        it is positive, negative, or zero.
-    *   *
-    *    * Return: Always 0.
-    *    **/
 		int n;
 
 			srand(time(0));
