871129e89b198c61268fb1ee7ab68bec4916b145
root <Diego29012>
2023-02-15 09:09:24 -0800

    Automatic

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,14 @@
+#include <stdio.h>
 
-puts("\"Programming is like building a multilingual puzzle");
+/**
+ *  * main - Prints "Programming is like building a multilingual
+ *   *                puzzle, followed by a new line.
+ *    *
+ *     * Return: Always 0.
+ *      */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+}
