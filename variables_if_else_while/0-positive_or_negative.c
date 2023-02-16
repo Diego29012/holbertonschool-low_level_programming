be748c78fce4e924439f29d4fd1831c4dea27937
root <Diego29012>
2023-02-16 04:23:09 -0800

    Automatic

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -8,8 +8,11 @@
  *    *
  *     * Return: Always 0.
  *     **/
+
 int main(void)
+
 {
+
 int n;			
 srand(time(0));
 n = rand() - RAND_MAX / 2;
@@ -19,4 +22,5 @@ else if (n < 0)
 printf("%d is negative\n", n);
 else										printf("%d is zero\n", n);
 return (0);
+
 }
