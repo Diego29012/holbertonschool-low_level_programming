ded93f9fca57f45279dfc4627f54d9dd02d3200e
root <Diego29012>
2023-02-16 08:51:07 -0800

    Automatic

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *   * main - Prints a serie of numbers with commas
+ *   *
+ *   * Return: Always (Success);
+ *   **/
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	putchar(x);
+	if (x != '9')
+	putchar(',');
+	putchar(' ');
+	putchar('\n');
+	return (0);
+}
