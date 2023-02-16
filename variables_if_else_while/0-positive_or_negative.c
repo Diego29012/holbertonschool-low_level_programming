2a6331aca0bd56abcb0955072a710b31b36dc425
root <Diego29012>
2023-02-16 05:10:04 -0800

    Automatic

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -16,8 +16,7 @@ int main (void)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n == 0)
-	
+	else if (n == 0)	
 {
 		printf("%d is zero\n", n);
 			}
@@ -25,6 +24,5 @@ int main (void)
 	{
 		printf("%d is negative\n", n);
 	}
-	
 		return (0);
 		}
