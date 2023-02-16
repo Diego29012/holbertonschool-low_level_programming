5611743a71b8774ef4618ac833cc37ee25eb048a
root <Diego29012>
2023-02-16 04:43:21 -0800

    Automatic

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -5,8 +5,9 @@
  *    * Return: Always (Success)
  *     **/
 int main(void)
+
 {		char c;
-			for (c = 'a'; c <= 'z'; c++);
+			for (c = 'a'; c <= 'z'; c++)
 						putchar(c);
 				putchar('\n');
 					return (0);
