470369bac6fb6b5f4d75560cacd09e4e9842dc39
root <Diego29012>
2023-02-16 04:36:00 -0800

    Automatic

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *  * main - Prints the alphabetic
  *   *
@@ -7,7 +6,7 @@
  *     **/
 int main(void)
 {	char c;
-			for (c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 						putchar(c);
 				putchar('\n');
 					return (0);
