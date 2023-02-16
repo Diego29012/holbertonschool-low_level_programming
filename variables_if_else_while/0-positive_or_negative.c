635d323698685a0fad7257e63e9e99a8a440d71e
root <Diego29012>
2023-02-16 04:51:17 -0800

    Automatic

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -4,12 +4,11 @@
 /**
  *  * main - Prints a random number and states whether
  *  * it is positive, negative, or zero.
- *   *
- *   * Return: Always 0.
- *   **/
-
+ *  *
+ *  * Return: Always 0.
+ *  **/
 int main(void)
-{int n;			
+{ int n;			
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
