d5fc0117badfb575d74d7cbffe463980a0deef31
root <Diego29012>
2023-02-16 09:54:39 -0800

    Automatic

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- *  *  * main - Prints "_putchar" followed by a new line.
- *   *   *
- *    *    * Return: Always 0.
- *     *     **/
+ *  * main - Prints "_putchar" followed by a new line.
+ *   *
+ *    * Return: Always 0.
+ *     */
 int main(void)
 {
-	puts("\_putchar");
+	_putchar('_');
+	_putchar('P');
+	_putchar('U');
+	_putchar('T');
+	_putchar('C');
+	_putchar('H');
+	_putchar('A');
+	_putchar('R');
+	_putchar('\n');
+
 	return (0);
 }
