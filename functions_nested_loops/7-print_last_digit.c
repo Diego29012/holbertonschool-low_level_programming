69365d6b23ba46d2bf078995651ca30e4ca5600e
root <Diego29012>
2023-02-17 09:54:02 -0800

    Automatic

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,13 @@
 **/
 int print_last_digit(int c)
 {
-int lastd;
-lastd = n % 10;
-if (lastd < 0)
-{
-lastd = lastd * -1;
+	int lastd;
+	lastd = c % 10;
+	if (lastd < 0)
+	{
+		lastd = lastd * -1;
+		_putchar(lastd + '0');
+		return (lastd);
 }
-_putchar(lastd + '0');
-return (lastd);
+		return (0);
 }
