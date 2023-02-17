99cb73ff92fac8175c4b608fc3262634236d8088
root <Diego29012>
2023-02-17 09:14:42 -0800

    Automatic

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,23 @@
 #include "main.h"
+#include <stdio.h>
+/**
+* *  print_last_digit - check the code
+* *  @c: integer
+* *  Return: Always c
+**/
+int print_last_digit(int c)
+{
+int lastd;
+lastd = c % 10;
+if (lastd > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", c, lastd);
+}
+else if (lastd == 0)
+printf("Last digit of %d is %d and is 0\n", c, lastd);
+{
+else if (lastd < 6 && lastd != 0)
+printf("Last digit of %d is %d and is less than 6 and not 0\n", c, lastd);
+}
+return (0);
+}
