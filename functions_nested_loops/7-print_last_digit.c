7ff9f43442b0bf6aa4a82d749a85b71d9757474a
root <Diego29012>
2023-02-17 09:16:33 -0800

    Automatic

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -11,13 +11,13 @@ int lastd;
 lastd = c % 10;
 if (lastd > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", c, lastd);
+printf("Last digit of %d \n", c, lastd);
 }
 else if (lastd == 0)
-printf("Last digit of %d is %d and is 0\n", c, lastd);
+printf("Last digit of %d \n", c, lastd);
 {
 else if (lastd < 6 && lastd != 0)
-printf("Last digit of %d is %d and is less than 6 and not 0\n", c, lastd);
+printf("Last digit of %d \n", c, lastd);
 }
 return (0);
 }
