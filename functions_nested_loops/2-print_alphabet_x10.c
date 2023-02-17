82d16cabbca1dd7e4864c1837f6d46bb28caa8ff
root <Diego29012>
2023-02-17 05:19:53 -0800

    Automatic

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+include "main.h"
+/**
+ *   * print_alphabet_x10 - Make alphabet x10 times
+ *   *
+ *   * Return: void
+ *   **/
+void print_alphabet_x10(void)
+{
+	char d;
+
+	int s = 0;
+
+	for (s <= 9)
+	for (d = 'a'; d <= 'z'; d++)
+	_putchar(d);
+	_putchar('\n');
+	s++;
+	return (0);
+}
+
