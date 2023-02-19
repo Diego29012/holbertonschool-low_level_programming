8238667f340f7c0b40da47fbcb0aa9d21eb963a8
root <Diego29012>
2023-02-19 15:39:32 -0800

    Automatic

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* * print_to_98 - Checks for alphabetic character
+* * @n: integer
+* **/
+void print_to_98(int n)
+{
+if (n >= 98)
+{
+for (n > 98)
+_putchar("%d, ", n--);
+_putchar("%d\n", n);
+}
+else
+{
+for (n < 98)
+_putchar("%d, ", n++);
+_putchar("%d\n", n);
+}
+}
