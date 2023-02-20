85a43d4bb163eba55aacf048981a472c9a042225
root <Diego29012>
2023-02-19 18:33:36 -0800

    Automatic

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* * print_to_98 - Checks for alphabetic characte
+* * print_to_98 - Prints all natural numbers from n to 98
 * * @n:The number to start printing from
 * * Return: Always 0.
 **/
