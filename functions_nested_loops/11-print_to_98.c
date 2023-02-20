4d274647f4b5347ba09b0697fcb161ef74c4be00
root <Diego29012>
2023-02-19 18:17:04 -0800

    Automatic

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,18 +8,32 @@
 void print_to_98(int n)
 {
 if (n < 98)
+{
 for (n <= 98)
+{
 _putchar("%d", n);
 if (n != 98)
+{
 _putchar(", ");
+}
 n++;
+}
+}
 else if (n > 98)
+{
 for (n >= 98)
+{
 _putchar("%d", n);
 if (n != 98)
+{
 _putchar(", ");
+}
 n--;
+}
+}
 else
+{
 _putchar("98");
+}
 ("\n");
 }
