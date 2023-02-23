eeaa8ca8a1c47e87dc49fb2982244e25027105d1
root <Diego29012>
2023-02-23 08:49:02 -0800

    automatic

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
 * * _isalpha - a function that prints the numbers, from 0 to 9
-* *  @c: integer
+* * @c: integer
 * * Return: 1 or 0
 **/
 int _isalpha(int c)
---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* * more_numbers - Print 10 times the numbers since 0 up to 14
+* * Return: 10 times of the numbers since 0 up to 10
+**/
+void more_numbers(void)
+{
+	int d, k;
+
+	for (d = 0; d < 10; d++)
+{
+	for (k = 0; k <= 14; k++)
+
+{
+	if (y > 9)
+
+{
+																putchar((y / 10) + '0');
+															}
+
+	putchar((y % 10) + '0');
+															}
+	putchar('\n');
+}
+
+}
