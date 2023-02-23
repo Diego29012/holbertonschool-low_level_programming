71dc1f4c5737db635eb35ffbcec4b16fbec6b3ff
root <Diego29012>
2023-02-23 06:05:56 -0800

    automatic

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
 * * _isalpha - a function that prints the numbers, from 0 to 9.
 * *  @c: integer
-* * Return: The numbers since 0 up to 9
+* * Return: The numbers since 0 and 9
 **/
 int _isalpha(int c)
+
 {
 	if (c >= 65 && c <= 122)
 {
