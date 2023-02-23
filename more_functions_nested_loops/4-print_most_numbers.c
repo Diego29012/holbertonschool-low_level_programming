ff68806d2bc46bc345b46366a3686289992706e9
root <Diego29012>
2023-02-23 06:04:13 -0800

    automati

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 /**
-*  * _isalpha - a function that prints the numbers, from 0 to 9, followed by a new line
-*  *  @c: integer
-*  * Return: 1 for alphabetic character or 0 for anything else
+* * _isalpha - a function that prints the numbers, from 0 to 9.
+* *  @c: integer
+* * Return: The numbers since 0 up to 9
 **/
 int _isalpha(int c)
 {
 	if (c >= 65 && c <= 122)
-{	
-       	return (1);
+{
+	return (1);
 }
 	return (0);
 }
