efb6bd3bdcdec4b503a3086effc7ded162c01aba
root <Diego29012>
2023-02-23 09:57:47 -0800

    automatic

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,29 +1,25 @@
 #include "stdio.h"
 #include "main.h"
 /**
-* * print_square - Prints n squares according n number of times
-* * @size: The number of squares
-* * Return:
-* **/
+* * print_square - print a square
+* * @size: integer
+* * Return: void
+**/
 void print_square(int size)
 {
 	int d, k;
 
-	if (size <= 0)
-{
-	putchar('\n');
-}
-	else
+	if (size > 0)
 {
 	for (d = 0; d < size; d++)
-
+{
 	for (k = 0; k < size; k++)
+{
 
-{       putchar(35);
+	putchar('#');
 
-}       putchar('\n');
-
+	putchar('\n');
 }
-
 }
-
+}
+}
