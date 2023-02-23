35b38c02ff283c3a9fc2c3f0c69c24bcbdf48a0b
root <Diego29012>
2023-02-23 14:29:48 -0800

    automatic

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* * main - function Fizz Buzz
+* * Return: always 0
+**/
+int main(void)
+{ int d;
+
+	for (d = 1; d <= 100; d++)
+
+	{
+		if (d % 3 == 0 && d % 5 == 0)
+		{
+		putchar ("FizzBuzz");
+		}
+		else if (d % 3 == 0)
+		{
+		puthcar ("Fizz");
+		}
+		else if (d % 5 == 0)
+		{
+		putchar ("Buzz");
+		}
+		else
+		{
+		putchar ("%d", d);
+		}
+		putchar ("\n");
+
+		return (0);
+}
