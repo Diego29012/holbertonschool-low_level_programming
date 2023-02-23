b6071d09b424e4b8b559c226a818e97b91706b99
root <Diego29012>
2023-02-23 05:32:27 -0800

    automatic

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,20 +2,16 @@
 #include "main.h"
 /**
 *  * print_numbers - that prints the numbers, from 0 to 9
-*  * @i: character
+*  * @c: character
 *  * Return: always 0
 *  **/
 void print_numbers(void)
 {
 	char c = '0'
 
-	for (c <= '9')
+	for (c <= '9'; c++)
 {
 	_putchar (c);
-	c++;
 }
 	_putchar ('\n');
-}
-{
-return (0);
 }
