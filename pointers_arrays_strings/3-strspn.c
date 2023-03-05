a4961f4915a4d470c7480ecc9b05db94c1d02343
root <Diego29012>
2023-03-05 13:27:41 -0800

    task 3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *_strspn - Write a function that gets the length of a prefix substring
+ * _strspn - Write a function that gets the length of a prefix substring
  * @s: the string to be searched
- *@accept: accept string
+ * @accept: accept string
  * Return: the number of bytes in the initial segment of s
  **/
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int a = 0, b, t = 0;
 
-	while (accept[a])
+while (accept[a])
 {
 	b = 0;
-		while (s[b] != 32)
+
+while (s[b] != 32)
 {
-			if (accept[a] == s[b]
+if (accept[a] == s[b])
 {
 t++;
 }
