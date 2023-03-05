25c07df83bed441c1138fe93fb313b0e29d7851e
root <Diego29012>
2023-03-05 12:06:02 -0800

    task 0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_memset - Write a function that fills memory with a constant byte
+ * @s: memory area to fill
+ * @b: constant byte to fill
+ * @n: bytes of memoty area to fill
+ * Return: a pointer to the memory area s
+**/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int d;
+
+for (d = 0; d < n; d++)
+	{
+s[d] = b;
+	}
+	return (s);
+}
