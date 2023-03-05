bde9f1f6b62bb46a3af465a6d907657956c39d2a
root <Diego29012>
2023-03-05 07:28:18 -0800

    task 6 fixed

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,8 +11,10 @@ char *cap_string(char *str)
 
 while (str[k])
 {
-	if (str[k] >= 97 && str[k] <= 122)
-	{
+	 while (str[k] >= 97 && str[k] <= 122)
+	 {
+		 k++;
+	 }
 		if (str[k - 1] == ' ' ||
 			str[k - 1] == '\t'    ||
 			str[k - 1] == '\n'    ||
@@ -29,6 +31,5 @@ while (str[k])
 			str[k] -= 32;
 			k++;
 	}
-}
 return (str);
 }
