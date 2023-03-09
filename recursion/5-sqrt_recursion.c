d5b81144d06cafd1706710a38093cd8d9026398f
root <Diego29012>
2023-03-09 09:51:15 -0800

    task 5-sqrt_recursion.c

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* * _sqrt_recursion - Write a function that returns
+* * @n: integer
+* * Return: int
+**/
+
+int _sqrt_recursion(int n)
+{
+return (_sqrt(n, 1));
+}
+
+/**
+* _sqrt - _sqrt_recursion
+* @n: integer
+* @i: integer
+* Return: sqrt
+**/
+
+int _sqrt(int n, int i)
+{
+
+if (n < 0)
+return (-1);
+if ((i * i) > n)
+return (-1);
+if (i * i == n)
+return (i);
+return (_sqrt(n, i + 1));
+}
