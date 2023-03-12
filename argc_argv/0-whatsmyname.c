0cda75a496426face504ab868597a50e6930b7e5
root <Diego29012>
2023-03-12 15:19:23 -0700

    task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints its name + \n
+ * @argc: number of arguments typed
+ * @argv: array poiting to arguments
+ * Return: always 0
+ **/
+int main( int argc, char *argv [])
+{
+
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
