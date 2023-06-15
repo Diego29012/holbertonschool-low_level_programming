fe7106c57dc172e7610438857d4bdb4d9a450296
Diego29012 <dieperey598@gmail.com>
2023-06-15 09:15:18 -0700

    task 4 puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-puts "Programming is like building a multilingual puzzle"
+include <stdio.h>
+/**
+ * main - start
+ * Return: always 0
+ */
+
+int main(void);
+{
+puts ("Programming is like building a multilingual puzzle");
+return (0);
+}
