65051f39813f70f39caf9b554338ab9f9b111738
Diego29012 <dieperey598@gmail.com>
2023-06-20 16:04:14 -0700

    task 7 print last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,13 +7,13 @@
 
 int print_last_digit(int d)
 {
-	int last_digit = x % 10;
+	int last_digit = d % 10;
 
 	if (last_digit < 0)
 	{
 		last_digit *= -1;
 	}
 
-	_putchar(lsat_digit + '0');
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
