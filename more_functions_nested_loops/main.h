fe03c57ce12a35d165831b7dc9b16017da6461c5
Diego29012 <dieperey598@gmail.com>
2023-06-20 19:21:00 -0700

    task 0

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void _putchar(char);
+
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+void print_triangle(int);
+void print_number(int n);
+void print_numbers(void);
