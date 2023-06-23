e969f9ac14b080a8a112d943e6a73ad195c1666e
Diego29012 <dieperey598@gmail.com>
2023-06-23 09:10:45 -0700

    task 0 98 reset

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,9 @@
 #include "main.h"
 
-/* reset_to_98 -takes a pointer to an int as parameter
+/*
+* reset_to_98 -takes a pointer to an int as parameter
+@n:integer
+Return: void
 */
 
 void reset_to_98(int *n)
