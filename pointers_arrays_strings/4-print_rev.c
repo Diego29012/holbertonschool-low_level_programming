df9d86f3b52d1eb6e231f60145fc81f9a05e26ce
Diego29012 <dieperey598@gmail.com>
2023-06-27 12:26:47 -0700

    task 4 print rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,14 +9,14 @@ void print_rev(char *s)
 {
 	int length = 0;
 
-	while (s[length] != '\0')
+	while (*(s + length) != '\0')
 	{
 	length++;
 	}
 
-	for (int i = length - 1; i >= 0; i--)
+	for (i = (length - 1;) i >= 0; i--)
 	{
-	putchar(s[i]);
+	putchar(*(s + i));
 	}
 	putchar('\n');
 }
