6ae919ef1bcfac6c2e4748e8146b5fb14e3b6f36
Diego29012 <dieperey598@gmail.com>
2023-06-27 19:21:51 -0700

    task 6 cap string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: input string
+ * Return: the pointer to dest
+ */
+char *cap_string(char *s)
+{
+	int i = 0, j = 0;
+	char c[] = {
+		',', ';', '.', '!', '?',
+		'"', '(', ')', '{', ' ',
+		'}', '\n', '\t'};
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= 32;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0 ; j < 13 ; j++)
+		{
+			if (c[j] == s[i - 1] && s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] -= 32;
+			}
+		}
+
+		i++;
+	}
+
+	return (s);
+}
