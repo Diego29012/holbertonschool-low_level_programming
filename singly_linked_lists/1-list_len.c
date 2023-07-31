44007530c4508cd11215e008e626dfcc29d5ee91
Diego29012 <dieperey598@gmail.com>
2023-07-30 19:41:21 -0700

    tasks

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* list_len - counts the number of elements
+* @h: pointer to the structure
+* Return: the number of elements
+**/
+size_t list_len(const list_t *h)
+{
+		int i = 0;
+
+		for (; h; i++)
+		{
+		h = h->next;
+		}
+		return (i);
+}
