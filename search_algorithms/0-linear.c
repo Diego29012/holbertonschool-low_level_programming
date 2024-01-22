f8e081e45142ec1c40e819589f9df22c8cc78ccf
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2024-01-21 23:19:36 -0300

    Update 0-linear.c

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -10,17 +10,17 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t n;
-	if (array != NULL)
-	{
-	for (n = 0; n < size; ++n)
-	{
-	printf("Value checked array[%ld] = [%d]\n", n, array[n]);
-	if (array[n] == value)
-	{
-	return (n);
-	}
-	}
-	}
-	return (-1);
+    size_t n;
+    if (array != NULL)
+    {
+        for (n = 0; n < size; ++n)
+        {
+            printf("Value checked array[%ld] = [%d]\n", n, array[n]);
+            if (array[n] == value)
+            {
+                return (n);
+            }
+        }
+    }
+    return (-1);
 }
