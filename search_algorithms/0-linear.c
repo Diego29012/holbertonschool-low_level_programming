43076941a852043278410e230e92b00e59ab7a07
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2024-01-21 23:54:40 -0300

    fixed 0-linear.c

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,16 +1,16 @@
 #include "search_algos.h"
 
 /**
-* linear_search - searches the list until a match is found or reaches the end
-* @array: is a pointer to the first element of the array
-* @size: is the number of elements in array
-* @value: is the value to search for
-* Return: n if it finds a match otherwise -1
-**/
-
+ * linear_search - searches the list until a match is found or reaches the end
+ * @array: is a pointer to the first element of the array
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ * Return: n if it finds a match otherwise -1
+ **/
 int linear_search(int *array, size_t size, int value)
 {
     size_t n;
+
     if (array != NULL)
     {
         for (n = 0; n < size; ++n)
